356662ca3d8dbc35a8af674ed1562ffde12cdd74
BublessPS <113979445+BublessPS@users.noreply.github.com>
2023-04-11 03:43:26 +0300

    Create Hit.java

---
+++ Hit.java
@@ -0,0 +1,17 @@
+package minegame159.OxwWareGui.utils.misc;
+
+import net.minecraft.class_239;
+import net.minecraft.class_243;
+
+public class MissHitResult
+extends class_239 {
+    public static final MissHitResult INSTANCE = new MissHitResult();
+
+    private MissHitResult() {
+        super(new class_243(0.0, 0.0, 0.0));
+    }
+
+    public class_239.class_240 method_17783() {
+        return class_239.class_240.field_1333;
+    }
+}
